8e87af6fe1081691c6f402eb5c87aaedda97ad0c
ives <ivesomardavid@gmail.com>
2024-05-03 05:49:31 -0400

    staging

--- .php_tmp/LocationsController.php
+++ .php_tmp/LocationsController.php
@@ -1,5 +1,5 @@
 <?php
-// src/Controller/RickMortyController.php
+// src/Controller/PostAjaxController.php
 
 namespace App\Controller;
 
@@ -8,11 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class RickMortyController extends AbstractController
+class LocationsController extends AbstractController
 {
-    #[Route('/RickMorty', name: 'RickMorty')]
-    public function ajax(): Response
-    {
-        return $this->render('Rick/RickMorty.html.twig');
-    }
+   #[Route('/location', name: 'location')]
+   public function ajax(): Response
+   {
+       return $this->render('Morty/location.html.twig');
+   }
 }
+
